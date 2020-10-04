1283f183a6f67be5e28a4cb0fb8258544e3bf40d
Lennert Franssens <lennert.franssens@gmail.com>
2020-10-04 13:40:06 +0200

    Update pre_installation.sh

--- pre_installation.sh
+++ pre_installation.sh
@@ -14,4 +14,23 @@ printf "\n${yel}[INFO]${end} Install snap\n"
 sudo dnf install snapd
 sudo ln -s /var/lib/snapd/snap /snap
 
+printf "\n${yel}[INFO]${end} Uninstall old versions of Docker\n"
+printf "\nReference: https://docs.docker.com/engine/install/fedora/\n"
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+printf "\n${yel}[INFO]${end} SET UP THE REPOSITORY for Docker\n"
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+
 printf "\n${yel}[INFO]${end} Log Out and proceed with install_programs.sh\n"
