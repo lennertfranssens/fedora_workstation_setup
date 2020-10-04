f0d17c89f52b639c1f1f66528cf91ce7bb0833e3
Lennert Franssens <lennert.franssens@gmail.com>
2020-10-04 18:02:49 +0200

    Update pre_installation.sh

--- pre_installation.sh
+++ pre_installation.sh
@@ -13,4 +13,13 @@ printf "\n${yel}[INFO]${end} Install snap\n"
 sudo dnf install snapd
 sudo ln -s /var/lib/snapd/snap /snap
 
-printf "\n${yel}[INFO]${end} Log Out and proceed with install_programs.sh\n"
+printf "\n${yel}[INFO]${end} Install git\n"
+sudo apt install git
+
+printf "\n${yel}[INFO]${end} Configure git\n"
+git config --global user.email "lennert.franssens@ugent.be"
+git config --global user.name "Lennert Franssens"
+echo | ssh-keygen -t rsa -b 4096 -C "lennert.franssens@ugent.be"
+printf "Enable SSH key on https://github.com/settings/keys with the SSH key generated in /home/lennertfranssens/.ssh/id_rsa.pub\n"
+
+printf "\n${yel}[INFO]${end} Enable ssh on git, log out and proceed with install_programs.sh\n"
