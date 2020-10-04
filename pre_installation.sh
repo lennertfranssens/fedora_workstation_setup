66d44d57714dfc469412aacdbb94bed2bcb80344
Lennert Franssens <lennert.franssens@gmail.com>
2020-10-04 13:24:51 +0200

    Create pre_installation.sh

---
+++ pre_installation.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+end=$'\e[0m'
+
+printf "|--------------------------------|\n"
+printf "|  Pre Fedora Workstation Setup  |\n"
+printf "|--------------------------------|\n"
+
+printf "\n${yel}[INFO]${end} Install snap\n"
+sudo dnf install snapd
+sudo ln -s /var/lib/snapd/snap /snap
+
+printf "\n${yel}[INFO]${end} Log Out and proceed with install_programs.sh\n"
